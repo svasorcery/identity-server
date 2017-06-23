d77770cce7a7bfb786bb9631bac75b677cf60cd5
Vladimir Sinyavsky <sva.sorcery@gmail.com>
2017-06-23 22:51:34 +0300

    closes #2

---
+++ Fiery.Api.Resources/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Fiery.Api.Resources
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.Title = "Resources API";
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseUrls("http://localhost:50201")
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
