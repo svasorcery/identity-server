f1bfc113d5d8df828eff4772a1acef4782d25093
Vladimir Sinyavsky <sva.sorcery@gmail.com>
2017-06-24 00:39:22 +0300

    closes #3

---
+++ Fiery.Identity.Clients.ClientCredentials/Program.cs
@@ -0,0 +1,43 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace Fiery.Identity.Clients.ClientCredentials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // discover endpoints from metadata
+            var disco = DiscoveryClient.GetAsync("http://localhost:50100").Result;
+
+            // request token
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "client.cc", "fiery_secret");
+            var tokenResponse = tokenClient.RequestClientCredentialsAsync("resources").Result;
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+
+            // call api
+            var client = new HttpClient();
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = client.GetAsync("http://localhost:50201/identity").Result;
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = response.Content.ReadAsStringAsync().Result;
+                Console.WriteLine(JArray.Parse(content));
+            }
+        }
+    }
+}
---
+++ Fiery.Identity.Clients.ResourceOwner/Program.cs
@@ -0,0 +1,28 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace Fiery.Identity.Clients.ResourceOwner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // discover endpoints from metadata
+            var disco = DiscoveryClient.GetAsync("http://localhost:50100").Result;
+
+            // request token
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "client.ro", "fiery_secret");
+            var tokenResponse = tokenClient.RequestResourceOwnerPasswordAsync("svasorcery", "gfhjkm123", "resources").Result;
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+        }
+    }
+}
