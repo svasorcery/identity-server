5cbbb900b5e158e156177ad7c85f24f3ad32b573
Vladimir <siniavsky.vladimir@gmail.com>
2017-07-26 17:38:16 +0300

    added HomeController with Secured area, closes #11

---
+++ Fiery.Identity.Clients.ImplicitMvc/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiery.Identity.Clients.ImplicitMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Secured()
+        {
+            return View();
+        }
+
+        public async Task Logout()
+        {
+            await HttpContext.Authentication.SignOutAsync("Cookies");
+            await HttpContext.Authentication.SignOutAsync("oidc");
+        }
+    }
+}
