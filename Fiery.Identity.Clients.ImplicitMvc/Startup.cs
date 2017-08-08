94e60f85366d287a3891c078ba86b6d56723644b
Vladimir <siniavsky.vladimir@gmail.com>
2017-08-08 18:04:29 +0300

    added MVC-client configuration and changed to hybrid flow

--- Fiery.Identity.Clients.ImplicitMvc/Startup.cs
+++ Fiery.Identity.Clients.ImplicitMvc/Startup.cs
@@ -8,11 +8,29 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.Extensions.Configuration;
 
 namespace Fiery.Identity.Clients.ImplicitMvc
 {
     public class Startup
     {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+
+            //System.Net.ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, errors) => true;
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
@@ -22,29 +40,44 @@ namespace Fiery.Identity.Clients.ImplicitMvc
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole();
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+            app.UseStaticFiles();
 
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
 
             app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
-                AuthenticationScheme = "Cookies"
+                AuthenticationScheme = CookieAuthenticationDefaults.AuthenticationScheme //"Cookies"
             });
 
             JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
 
             app.UseOpenIdConnectAuthentication(new OpenIdConnectOptions
             {
-                AuthenticationScheme = "oidc",
-                SignInScheme = "Cookies",
+                AuthenticationScheme = OpenIdConnectDefaults.AuthenticationScheme, //"oidc"
+                SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme,  //"Cookies"
 
-                Authority = "http://localhost:50100",
+                Authority = Configuration["oidc:Authority"],
                 RequireHttpsMetadata = false,
 
-                ClientId = "client.imp",
+                ClientId = Configuration["oidc:ClientId"],
+                ClientSecret = Configuration["oidc:ClientSecret"],
+
+                ResponseType = OpenIdConnectResponseType.CodeIdToken,
+                Scope = { Configuration["oidc:Scope"], "offline_access" },
+
+                GetClaimsFromUserInfoEndpoint = true,
                 SaveTokens = true
             });
 
