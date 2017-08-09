edd0c3e2b2cb1764cf4b9f430d8eaed381b40006
Vladimir <siniavsky.vladimir@gmail.com>
2017-08-09 10:27:18 +0300

    added styles in MVC-client

---
+++ Fiery.Identity.Clients.ImplicitMvc/wwwroot/_references.js
@@ -0,0 +1,4 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="js/site.js" />
+/// <reference path="lib/bootstrap/dist/js/bootstrap.min.js" />
+/// <reference path="lib/jquery/dist/jquery.min..js" />
