65135a46668306e48f946f5acc2370766af7909d
Vladimir <siniavsky.vladimir@gmail.com>
2017-08-09 10:30:16 +0300

    added mvc client Secured area

--- Fiery.Identity.Clients.ImplicitMvc/Controllers/HomeController.cs
+++ Fiery.Identity.Clients.ImplicitMvc/Controllers/HomeController.cs
@@ -2,14 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using IdentityModel.Client;
+using Microsoft.Extensions.Options;
+using Fiery.Identity.Clients.ImplicitMvc.Models;
 
 namespace Fiery.Identity.Clients.ImplicitMvc.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: /<controller>/
+        private readonly OpenIdConnectAuthenticationOptions _oidcOptions;
+
+        public HomeController(IOptions<OpenIdConnectAuthenticationOptions> optionsAccessor)
+        {
+            _oidcOptions = optionsAccessor.Value;
+        }
+
+
         public IActionResult Index()
         {
             return View();
@@ -18,13 +33,47 @@ namespace Fiery.Identity.Clients.ImplicitMvc.Controllers
         [Authorize]
         public IActionResult Secured()
         {
+            ViewData["Message"] = "Secure page.";
+
             return View();
         }
 
-        public async Task Logout()
+        public IActionResult Error()
         {
-            await HttpContext.Authentication.SignOutAsync("Cookies");
-            await HttpContext.Authentication.SignOutAsync("oidc");
+            return View();
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.Authentication.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            await HttpContext.Authentication.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
+
+            return Redirect("~/");
+        }
+
+        public async Task<IActionResult> CallApiUsingClientCredentials()
+        {
+            var tokenClient = new TokenClient($"{_oidcOptions.Authority}/connect/token", _oidcOptions.ClientId, _oidcOptions.ClientSecret);
+            var tokenResponse = await tokenClient.RequestClientCredentialsAsync(_oidcOptions.Scope);
+
+            var client = new HttpClient();
+            client.SetBearerToken(tokenResponse.AccessToken);
+            var content = await client.GetStringAsync($"{_oidcOptions.Api}/identity");
+
+            ViewBag.Json = JArray.Parse(content).ToString();
+            return View("json");
+        }
+
+        public async Task<IActionResult> CallApiUsingUserAccessToken()
+        {
+            var accessToken = await HttpContext.Authentication.GetTokenAsync("access_token");
+
+            var client = new HttpClient();
+            client.SetBearerToken(accessToken);
+            var content = await client.GetStringAsync($"{_oidcOptions.Api}/identity");
+
+            ViewBag.Json = JArray.Parse(content).ToString();
+            return View("json");
         }
     }
 }
