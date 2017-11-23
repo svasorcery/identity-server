31ce6974301964451a959248fffed77273ec1600
Vladimir Sinyavsky <siniavsky.vladimir@gmail.com>
2017-11-23 13:34:52 +0300

    removed Users.AspNetCore project

--- Fiery.Identity.Users.AspNetCore/Models/ManageViewModels/FactorViewModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Fiery.Identity.Users.AspNetCore.Models.ManageViewModels
-{
-    public class FactorViewModel
-    {
-        public string Purpose { get; set; }
-    }
-}
--- Fiery.Identity.Users.AspNetCore/Services/IEmailSender.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace Fiery.Identity.Users.AspNetCore.Services
-{
-    public interface IEmailSender
-    {
-        Task SendEmailAsync(string email, string subject, string message);
-    }
-}
